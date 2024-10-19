eb655c7241729b1d5b858d7075c2ad9fcba94e37
giladrv <giladrv@gmail.com>
2024-10-19 12:14:04 +0300

    api gw mgmt post to conn

--- agm.py
+++ agm.py
@@ -1,12 +1,28 @@
+# Standard
+import json
 # External
 import boto3
+from botocore.exceptions import ClientError
 
 CLIENT_NAME = 'apigatewaymanagementapi'
 
 class AGM():
 
-    def __init__(self, client = None):
+    def __init__(self, client = None, **kwargs):
         if client is not None:
             self.client = client
         else:
-            self.client = boto3.client(CLIENT_NAME)
+            self.client = boto3.client(CLIENT_NAME, **kwargs)
+
+    def post(self, conn_id: str, data):
+        try:
+            self.client.post_to_connection(
+                ConnectionId = conn_id,
+                Data = json.dumps(data, default = str))
+            return True
+        except ClientError as e:
+            print('ERR', json.dumps(e.response))
+            if e.response['Error']['Code'] == 'GoneException':
+                return False
+            else:
+                raise
