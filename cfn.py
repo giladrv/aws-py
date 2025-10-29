f813bdcdd0b42520fcb25115cefed3c9806a712b
giladrv <giladrv@gmail.com>
2025-10-29 11:20:32 +0200

    update invoke dest yml

--- cfn.py
+++ cfn.py
@@ -10,7 +10,7 @@ from botocore.exceptions import ClientError
 
 EVENTS_URL = 'https://{region}.console.aws.amazon.com/cloudformation/home?region={region}#/stacks/events?stackId={stack}'
 
-INVOKE_DEST = "{ Destination: !Ref {resource} }"
+INVOKE_DEST_YML = "{ Destination: !Sub '{arn}' }"
 LAYER_PREFIX = "arn:aws:lambda:${AWS::Region}:${AWS::AccountId}:layer"
 NO_VALUE = "!Ref AWS::NoValue"
 
